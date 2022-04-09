f61f92c23c53a84b4d31e15b93de7f0459bfc0eb
Morfi <marfutin.00@mail.ru>
2022-04-09 08:34:51 +0300

    Added lesson 2 HW

--- Homeworks/Lesson_2_Homework/5. Rating.py
+++ Homeworks/Lesson_2_Homework/5. Rating.py
@@ -1,13 +1,13 @@
-# my_list = [7, 5, 3, 3, 2]
-#
-# el = int(input('Введите число: \n'))
-#
-# if el in my_list:
-#     my_list.insert((my_list.index(el)), el)
-# elif el > my_list[0]:
-#     my_list.insert(0, el)
-# elif el < my_list[-1]:
-#     my_list.append(el)
-#
-# print(my_list)
+my_list = [7, 5, 3, 3, 2]
+
+el = int(input('Введите число: \n'))
+
+if el in my_list:
+    my_list.insert((my_list.index(el)), el)
+elif el > my_list[0]:
+    my_list.insert(0, el)
+elif el < my_list[-1]:
+    my_list.append(el)
+
+print(my_list)
 
